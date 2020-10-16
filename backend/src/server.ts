06fc8764658f7d508b890973a2d4eea3494b5deb
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2020-10-16 19:32:13 -0300

    Configuration node js for application initial - Basic

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    return response.json({message: 'Hello World'});
+});
+
+app.listen(3333);
+
+
+// REQUISIÇÃO E RESPOSTA clear NO NODE JS
